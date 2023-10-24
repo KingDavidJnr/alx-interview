835cd35bb1c919dee052c70c0b679ad0e1f691e1
David Oduse <odusedavid@gmail.com>
2023-10-25 00:45:22 +0100

    fix: updated the comments in the utf8 algorithm file

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 """
-UTF-8 Validation
+UTF-8 Validation Algorithm
 """
 
 
 def validUTF8(data):
     """
-    data: a list of integers
+    data: list of integers
     Return: True if data is a valid UTF-8
     encoding, else return False
     """
